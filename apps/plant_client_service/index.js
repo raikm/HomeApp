f69deb09dd2d06fa251df9ac788877437ee37387
Raik <raik.mueller@auros-consulting.de>
2024-06-22 17:47:33 +0200

    feat: plant client service

---
+++ apps/plant_client_service/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const exec = require('child_process').exec
+const port = 3000
+
+app.post('/run-script', (req, res) => {
+  exec('sudo node /GitHub/nevo/apps/plant_client/dist/src/index.js', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return res.status(500).send(`Error: ${error}`)
+    }
+    res.send(`stdout: ${stdout}\nstderr: ${stderr}`)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Plant client server listening at http://localhost:${port}`)
+})
